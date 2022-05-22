73f6b1aaf664571e67e7d58a877f98cd1b157cbc
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2022-05-23 00:06:45 +0900

    Initial Commit

---
+++ OriginMockApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OriginMockApp
+//
+//  Created by Hiroaki-Hirabayashi on 2022/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
