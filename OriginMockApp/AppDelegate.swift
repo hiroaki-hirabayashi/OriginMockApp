d7b5b83c03a4b301e9b34629b6318a60f2f70b05
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2022-06-01 21:58:45 +0900

    TabBarController作成

--- OriginMockApp/AppDelegate.swift
+++ OriginMockApp/AppDelegate.swift
@@ -12,6 +12,8 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
+    /** トップのタブバーコントローラ */
+    var topTabBarController: TabBarController?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
