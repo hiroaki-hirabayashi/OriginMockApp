91bfe76d243227cff2190384645aacb3e6094052
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2022-11-21 00:55:44 +0900

    collectionView作成

--- OriginMockApp/ViewController/Top/TopViewController.swift
+++ OriginMockApp/ViewController/Top/TopViewController.swift
@@ -7,13 +7,52 @@
 
 import UIKit
 
+/// Top画面
 final class TopViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet private weak var collectionView: UICollectionView!
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Top"
+        let cellName = String(describing: TopCollectionViewCell.self)
+        collectionView.register(UINib(nibName: cellName, bundle: nil), forCellWithReuseIdentifier: TopCollectionViewCell.cellId)
     }
 
+    // MARK: - IBActions
+}
 
+// MARK: - UICollectionViewDelegate, UICollectionViewDataSource
+extension TopViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+//    func numberOfSections(in collectionView: UICollectionView) -> Int {
+//        return 1
+//    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell:UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TopCollectionViewCell.cellId, for: indexPath) as? TopCollectionViewCell else {
+            fatalError("TopCollectionViewCell読み込み失敗")
+        }
+        return cell
+    }
+
+    
+    
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension TopViewController: UICollectionViewDelegateFlowLayout {
+
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+      return collectionView.bounds.size
+  }
 }
 
