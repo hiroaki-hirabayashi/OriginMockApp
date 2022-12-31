ea1d647721db96705400c1c8055a303126bf7fea
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2023-01-01 06:30:43 +0900

    タップ毎にキープボタンのUIを変更する

--- OriginMockApp/View/JobList/JobListCell.swift
+++ OriginMockApp/View/JobList/JobListCell.swift
@@ -13,12 +13,22 @@ final class JobListCell: UITableViewCell {
     // MARK: - Properties
     let jobList = DataStore.shared.jobList.prefix(10)
     static let cellId = "JobListCell"
-    
+    private var isKeep = false
     // MARK: - IBOutlets
     @IBOutlet private weak var companyNameLabel: UILabel!
-    
     @IBOutlet private weak var keepButton: UIButton!
     
+    // MARK: - IBActions
+    @IBAction func tappedKeepButton(_ sender: Any) {
+        isKeep.toggle()
+        if isKeep {
+            keepButton.setTitle("キープ済み", for: .normal)
+            keepButton.backgroundColor = .systemYellow
+        } else {
+            keepButton.setTitle("キープボタン", for: .normal)
+            keepButton.backgroundColor = .white
+        }
+    }
     // MARK: - LifeCycle
     override func awakeFromNib() {
         super.awakeFromNib()
