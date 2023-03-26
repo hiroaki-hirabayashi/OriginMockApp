8b65f7237164d0ab86eb2f0030825fca379a01c2
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2023-03-26 23:41:54 +0900

    キープリスト用Cell作成開始

--- OriginMockApp/View/KeepList/KeepListCell.swift
+++ OriginMockApp/View/KeepList/KeepListCell.swift
@@ -10,4 +10,25 @@ import UIKit
 /// キープリストCell
 final class KeepListCell: UITableViewCell {
     
+    // MARK: - Properties
+    static let cellId = "KeepListCell"
+    
+    // MARK: - IBOutlets
+    @IBOutlet private weak var keepedDateLabel: UILabel!
+    @IBOutlet private weak var companyNameLabel: UILabel!
+    
+    // MARK: - LifeCycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+     
+    }
+    
+    // MARK: - Internal Functions
+    func setCompanyNameLabel(companyName: String) {
+        companyNameLabel.text = companyName
+    }
+    
+    func setKeepedDate(date: String) {
+        keepedDateLabel.text = date
+    }
 }
