80cae802793d10dc345ee5ab0cbe42e2dcd9ac83
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2023-04-08 23:46:23 +0900

    キープリストにキープロジック追加

--- OriginMockApp/ViewController/KeepList/KeepListViewController.swift
+++ OriginMockApp/ViewController/KeepList/KeepListViewController.swift
@@ -10,8 +10,23 @@ import UIKit
 /// キープリスト画面
 final class KeepListViewController: UIViewController {
     
+    // MARK: - Properties
+    private let jobList = DataStore.shared.jobList
+    private let keepList = DataStore.shared.keepList
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var sumLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.title = "KeepList"
+        navigationItem.title = "キープリスト"
+        tableView.register(UINib(nibName: KeepListCell.cellId, bundle: nil), forCellReuseIdentifier: KeepListCell.cellId)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        sumLabel.text = "キープした求人\(DataStore.shared.keepList.count)件表示"
+        tableView.reloadData()
     }
 }
